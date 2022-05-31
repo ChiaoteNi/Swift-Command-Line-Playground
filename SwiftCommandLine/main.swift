eeb7f54a00337386d2a4350afde41587bdb5bbf4
Chiaote Ni <aaron.chiao.19@gmail.com>
2022-05-31 21:11:31 +0800

    3. How to read/write Environments

--- SwiftCommandLine/main.swift
+++ SwiftCommandLine/main.swift
@@ -10,3 +10,19 @@ print("Hello, World!")
 let arguments = ProcessInfo.processInfo.arguments
 print("💥arguments =", arguments)
 print("💥CommandLine.arguments = ", CommandLine.arguments)
+
+// 3. How to read / write Environments:
+// Read
+let environments = ProcessInfo.processInfo.environment
+print("💥environments =", environments)
+print("💥getENV =", String(utf8String: getenv("HOME"))!)
+print("💥PATH =", String(describing: environments["PATH"]))
+
+// Write
+if let rawValue = getenv("YO") {
+    print("💥getENVYO =", String(utf8String: rawValue) ?? "nil")
+}
+setenv("YO", "yo123", 1)
+if let rawValue = getenv("YO") {
+    print("💥getENVYO =", String(utf8String: rawValue) ?? "nil")
+}
