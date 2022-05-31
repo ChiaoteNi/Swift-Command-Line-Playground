174eab86895ddbbcfc382b01798eb57ca01b4f43
Chiaote Ni <aaron.chiao.19@gmail.com>
2022-05-31 21:11:13 +0800

    2. How to read the arguments

--- SwiftCommandLine/main.swift
+++ SwiftCommandLine/main.swift
@@ -2,3 +2,11 @@ import Foundation
 
 // 1. How to echo:
 print("Hello, World!")
+
+// 2. How to read the arguments:
+// ex: SwiftCommandLine -path "www.google.com"
+// arguments = [currentPath, "-path", "www.google.com"]
+// Now you know how --help works
+let arguments = ProcessInfo.processInfo.arguments
+print("💥arguments =", arguments)
+print("💥CommandLine.arguments = ", CommandLine.arguments)
